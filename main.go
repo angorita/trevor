0b0cf8fe7d8513078b2e83e99837f81ea83cbe54
angorita <oaangarita@yahoo.com.ar>
2026-08-15 10:50:36 -0300

    sabado

--- main.go
+++ main.go
@@ -2,37 +2,10 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
-
-// Home es el manejador para la página de inicio
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.html")
-}
-
-// About es el manejador para la página sobre nosotros
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.html")
-}
-
-// renderTemplate ahora recibe el nombre del archivo template como string
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, err := template.ParseFiles("./templates/" + tmpl)
-	if err != nil {
-		fmt.Println("Error al leer el archivo template:", err)
-		http.Error(w, "Error interno del servidor", http.StatusInternalServerError)
-		return
-	}
-
-	err = parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Error al ejecutar el template:", err)
-		return
-	}
-}
 
 func main() {
 	http.HandleFunc("/", Home)
---
+++ render.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+// renderTemplate ahora recibe el nombre del archivo template como string
+func renderTemplate(w http.ResponseWriter, html string) {
+	parsedTemplate, err := template.ParseFiles("./templates/" + html)
+	if err != nil {
+		fmt.Println("Error al leer el archivo template:", err)
+		http.Error(w, "Error interno del servidor", http.StatusInternalServerError)
+		return
+	}
+
+	err = parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error al ejecutar el template:", err)
+		return
+	}
+}
